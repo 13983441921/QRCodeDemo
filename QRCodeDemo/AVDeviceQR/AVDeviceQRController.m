30aa63838d8cdc085a82e586a1d843c8a308627f
好迪 <ihaodi@icloud.com>
2016-03-10 19:21:57 +0800

    原生 扫描 API ok

--- QRCodeDemo/AVDeviceQR/AVDeviceQRController.m
+++ QRCodeDemo/AVDeviceQR/AVDeviceQRController.m
@@ -7,33 +7,201 @@
 //
 
 #import "AVDeviceQRController.h"
+#import "QRScanView.h"
 
 #import <AVFoundation/AVFoundation.h>
 
-@interface AVDeviceQRController ()
+
+@interface AVDeviceQRController ()<AVCaptureMetadataOutputObjectsDelegate>
 
 @property (nonatomic , strong)AVCaptureDevice *captureDevice;
 @property (nonatomic , strong)AVCaptureSession *captureSession;
-@property (nonatomic , strong)AVCaptureInput *captureInput;
+@property (nonatomic , strong)AVCaptureDeviceInput *captureDeviceInput;
 @property (nonatomic , strong)AVCaptureMetadataOutput *captureMetadataOutput;
+@property (nonatomic , strong)AVCaptureVideoPreviewLayer *previewLayer;
+@property (nonatomic , strong)AVCaptureConnection *captureConnect;
+
+/**
+ *  @author Haodi, 16-03-10 16:03:57
+ *
+ *  扫描区域
+ */
+@property (nonatomic, assign) CGRect scanRect;
+
+@property (nonatomic, assign) BOOL isQRCodeCaptured;
 
 @end
 
 @implementation AVDeviceQRController
 
+- (void)dealloc{
+    
+}
 
 - (void)viewDidLoad{
     [super viewDidLoad];
-    
+    self.scanRect = CGRectMake(60.0f, 100.0f, 200.0f, 200.0f);
+
 }
 
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
+    
+    [self requestAuthCamera];
+    self.isQRCodeCaptured = NO;
 }
 
 - (void)viewDidDisappear:(BOOL)animated{
     [super viewDidDisappear:animated];
+    
+    [self stopCapture];
 }
 
 #pragma mark -
+// 请求相机权限
+- (void)requestAuthCamera{
+    __weak typeof(self)weakSelf = self;
+    AVAuthorizationStatus authorizationStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+    switch (authorizationStatus) {
+        case AVAuthorizationStatusNotDetermined:{
+            [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+                if (granted) {
+                    [weakSelf configDeviceQR];
+                }
+            }];
+            break;
+        }
+        case AVAuthorizationStatusAuthorized:
+            NSLog(@"已授权");
+            [self configDeviceQR];
+            break;
+        case AVAuthorizationStatusRestricted:
+            NSLog(@"受限，有可能开启了访问限制");
+        case AVAuthorizationStatusDenied:
+            NSLog(@"访问受限");
+            [self loadNoAuthCameraView];
+            break;
+        default:
+            break;
+    }
+    
+}
+
+- (void)configDeviceQR{
+    NSError *error;
+    AVCaptureDeviceInput *deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:self.captureDevice error:&error];
+    if (error == nil) {
+        if ([self.captureSession canAddInput:deviceInput]) {
+            [self.captureSession  addInput:deviceInput];
+        }
+        
+        if ([self.captureSession canAddOutput:self.captureMetadataOutput]) {
+             // 这行代码要在设置 metadataObjectTypes 前
+            [self.captureSession addOutput:self.captureMetadataOutput];
+        }
+        self.captureMetadataOutput.metadataObjectTypes = @[AVMetadataObjectTypeQRCode];
+        //    self.captureOutput.metadataObjectTypes = @[AVMetadataObjectTypeQRCode,AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode128Code];
+
+        _previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.captureSession];
+        _previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+        _previewLayer.frame = self.view.frame;
+        [self.view.layer insertSublayer:_previewLayer atIndex:0];
+        
+        __weak typeof(self) weakSelf = self;
+        [[NSNotificationCenter defaultCenter] addObserverForName:AVCaptureInputPortFormatDescriptionDidChangeNotification
+                                                          object:nil
+                                                           queue:[NSOperationQueue currentQueue]
+                                                      usingBlock: ^(NSNotification *_Nonnull note) {
+                                                          weakSelf.captureMetadataOutput.rectOfInterest = [_previewLayer metadataOutputRectOfInterestForRect:weakSelf.scanRect]; // 如果不设置，整个屏幕都可以扫
+                                                          //!!!!!!!!!! 与直接设置的区别 还未发现有啥区别
+                                                      }];
+       
+
+        AVCaptureStillImageOutput *img =  [self.captureSession.outputs objectAtIndex:0];
+        _captureConnect =[img connectionWithMediaType:AVMediaTypeVideo];
+
+        QRScanView *scanView = [[QRScanView alloc] initWithScanRect:self.scanRect];
+        [self.view addSubview:scanView];
+        
+        [self startCapture];
+        
+    }
+    else {
+        NSLog(@"err = %@",error);
+    }
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+          [self.previewLayer setAffineTransform:CGAffineTransformMakeScale(1.5, 1.5)];
+        _captureConnect.videoScaleAndCropFactor = 1.5;
+
+           });
+}
+
+- (void)startCapture{
+    [self.captureSession startRunning];
+}
+
+- (void)stopCapture{
+    [self.captureSession stopRunning];
+}
+
+- (void)loadNoAuthCameraView{
+    NSString *cameraTitle = [NSString stringWithFormat:@"请在iPhone的“设置-隐私-相机”选项中，允许【%@】访问您的相机",[[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleDisplayName"]];
+    [self showAlertViewWithMessage:cameraTitle];
+}
+
+
+#pragma mark - Private Methods
+
+- (void)showAlertViewWithMessage:(NSString *)message {
+    NSLog(@"%@", message);
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"" message:message delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil];
+    alertView.delegate = self;
+    [alertView show];
+}
+
+#pragma mark -
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection{
+    AVMetadataMachineReadableCodeObject *metadataObject = metadataObjects.firstObject;
+    if ([metadataObject.type isEqualToString:AVMetadataObjectTypeQRCode]&& !self.isQRCodeCaptured) {
+        self.isQRCodeCaptured = YES;
+        [self stopCapture];
+        [self showAlertViewWithMessage:metadataObject.stringValue];
+    }
+}
+
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex {
+    
+}
+
+#pragma mark -
+-(AVCaptureDevice *)captureDevice{
+    if (_captureDevice == nil) {
+        _captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+        //更改这个设置的时候必须先锁定设备，修改完后再解锁，否则崩溃
+//        [_captureDevice lockForConfiguration:nil];
+//        //设置闪光灯为自动
+//        [_captureDevice setFlashMode:AVCaptureFlashModeAuto];
+//    
+//        [_captureDevice unlockForConfiguration];
+    }
+    return _captureDevice;
+}
+
+-(AVCaptureSession *)captureSession{
+    if (_captureSession == nil) {
+        _captureSession = [[AVCaptureSession alloc] init];
+        [_captureSession setSessionPreset:AVCaptureSessionPresetInputPriority];
+    }
+    return _captureSession;
+}
+
+- (AVCaptureMetadataOutput *)captureMetadataOutput
+{
+    if (_captureMetadataOutput == nil) {
+        _captureMetadataOutput = [[AVCaptureMetadataOutput alloc] init];
+        [_captureMetadataOutput setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    }
+    return _captureMetadataOutput;
+}
 @end
